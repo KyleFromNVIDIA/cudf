a4ae586065d21515abb06435793646d50387e8e4
David Wendt <dwendt@nvidia.com>
2026-08-12 15:44:06 -0400

    move the page_index_filter.cu sync call

--- cpp/src/io/parquet/experimental/page_index_filter.cu
+++ cpp/src/io/parquet/experimental/page_index_filter.cu
@@ -425,7 +425,7 @@ struct page_stats_caster : public stats_caster_base {
       // Construct a row indices mapping based on page row offsets.
       auto const page_indices = compute_page_indices_async(
         page_row_offsets, total_rows, stream, cudf::get_current_device_resource_ref());
-
+      stream.synchronize();
       // For non-strings columns, directly gather the page-level column data and bitmask to the
       // row-level.
       if constexpr (not cuda::std::is_same_v<T, cudf::string_view>) {
@@ -584,13 +584,14 @@ struct page_stats_to_row_mask_converter : public page_stats_caster {
 
       auto const page_mask_nullmask =
         page_mask->null_count()
-          ? cudf::detail::make_host_vector(
+          ? cudf::detail::make_host_vector_async(
               cudf::device_span<bitmask_type const>{
                 page_mask->view().null_mask(),
                 static_cast<std::size_t>(num_bitmask_words(page_mask->size()))},
               stream)
           : cudf::detail::make_empty_host_vector<bitmask_type>(0, stream);
 
+      stream.synchronize();
       auto [row_mask_data, row_mask_bitmask] =
         build_data_and_nullmask<bool>(page_mask->mutable_view(),
                                       page_mask_nullmask.data(),
